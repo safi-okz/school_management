c1776e583ea464de237ffdf6109532fa01ef4b12
safi-okz <safi90655@gmail.com>
2025-09-07 02:15:13 +0500

    lesson page completed

--- src/app/(dashboard)/list/lessons/page.tsx
+++ src/app/(dashboard)/list/lessons/page.tsx
@@ -6,34 +6,27 @@ import Image from "next/image";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import Link from "next/dist/client/link";
-import { role, classesData } from "@/lib/data";
+import { role, lessonsData } from "@/lib/data";
 
-type Class = {
+type Lesson = {
   id: string;
-  name: string;
-  capacity: number;
-  grade: number;
-  supervisor: string;
+  subject: string;
+  class: string;
+  teacher: string;
 };
 
 const columns = [
   {
-    header: "Class Name",
-    accessor: "name",
+    header: "Subject Name",
+    accessor: "subject",
   },
   {
-    header: "Capacity",
-    accessor: "capacity",
-    className: "hidden md:table-cell",
+    header: "Class",
+    accessor: "class",
   },
   {
-    header: "Grade",
-    accessor: "grade",
-    className: "hidden md:table-cell",
-  },
-  {
-    header: "Supervisor",
-    accessor: "supervisor",
+    header: "Teacher",
+    accessor: "teacher",
     className: "hidden md:table-cell",
   },
   {
@@ -42,30 +35,29 @@ const columns = [
   },
 ];
 
-function ClassesList() {
-  const renderRow = (row: Class) => (
+function LessonsList() {
+  const renderRow = (row: Lesson) => (
     <tr
       key={row.id}
       className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
     >
       <td className="flex items-center gap-4 p-4">
         <div className="flex flex-col">
-          <span className="font-semibold">{row.name}</span>
+          <span className="font-semibold">{row.subject}</span>
         </div>
       </td>
-      <td className="hidden md:table-cell gap-4 p-4">{row.capacity}</td>
-      <td className="hidden md:table-cell gap-4 p-4">{row.grade}</td>
-      <td className="hidden md:table-cell gap-4 p-4">{row.supervisor}</td>
+      <td className="hidden md:table-cell gap-4 p-4">{row.class}</td>
+      <td className="hidden md:table-cell gap-4 p-4">{row.teacher}</td>
       <td>
         <div className="flex items-center gap-4 p-4">
           <Link href={`/teachers/${row.id}`}>
             <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
-              <Image src="/view.png" height={16} width={16} alt="View" />
+              <Image src="/edit.png" height={16} width={16} alt="Edit" />
             </button>
           </Link>
           {role == "admin" && (
             <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
-              <Image src="/delete.png" height={16} width={16} alt="View" />
+              <Image src="/delete.png" height={16} width={16} alt="Delete" />
             </button>
           )}
         </div>
@@ -77,7 +69,7 @@ function ClassesList() {
     <div className="p-4 bg-white rounded-md flex-1 m-4">
       {/* top */}
       <div className="flex justify-between items-center">
-        <h1 className="hidden md:block text-lg font-semibold">All Classes</h1>
+        <h1 className="hidden md:block text-lg font-semibold">All Lessons</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-3 self-end">
@@ -94,11 +86,11 @@ function ClassesList() {
         </div>
       </div>
       {/* top */}
-      <Table columns={columns} renderRow={renderRow} data={classesData} />
+      <Table columns={columns} renderRow={renderRow} data={lessonsData} />
       {/* bottom */}
       <Pagination />
     </div>
   );
 }
 
-export default ClassesList;
+export default LessonsList;
